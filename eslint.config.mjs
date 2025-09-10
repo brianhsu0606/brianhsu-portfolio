34cf75fab673a16d020cd8a46ea80f08496a0abb
Brian Hsu <a0938406813@gmail.com>
2025-09-11 01:32:24 +0800

    chore: install sass, tailwind CSS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,24 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
+export default withNuxt({
   // Your custom configs here
-)
+
+  rules: {
+    // 關閉 multi-word 提示
+    "vue/multi-word-component-names": "off",
+    // 允許 void 元素自閉合（input）
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+          component: "always",
+        },
+        svg: "always",
+        math: "always",
+      },
+    ],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="bg-blue-200">Brian Hsu</header>
+</template>
+
+<style scoped lang="scss"></style>
