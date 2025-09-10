1f7d39c6aa4c43a07b57d0fc193a9241108a0c6c
Brian Hsu <a0938406813@gmail.com>
2025-09-11 01:36:46 +0800

    feat: add header

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header class="bg-blue-200">Brian Hsu</header>
+  <header class="flex justify-between items-center px-8 h-[10vh] text-2xl font-bold bg-blue-200">
+    <h3>Brian Hsu</h3>
+    <nav>
+      <ul class="flex gap-8">
+        <li><NuxtLink to="#home">HOME</NuxtLink></li>
+        <li><NuxtLink to="#about">ABOUT</NuxtLink></li>
+        <li><NuxtLink to="#project">PROJECTS</NuxtLink></li>
+        <li><NuxtLink to="#contact">CONTACT</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <style scoped lang="scss"></style>
