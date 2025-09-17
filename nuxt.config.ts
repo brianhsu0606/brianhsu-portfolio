33c9d45a84e0519921db0abd5b31708a5434b0c1
Brian Hsu <a0938406813@gmail.com>
2025-09-17 23:00:41 +0800

    chore: remonve nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,6 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
   css: ["~/assets/css/main.scss"],
 });
